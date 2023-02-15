6fd943184055d139470e0c380ac63ea60bc65752
Julia Andronowitz <juliaandronowitz@Julias-MacBook-Air.local>
2023-02-14 20:43:52 -0500

    updates to figures and code for svc

--- Code/svc_data.R
+++ Code/svc_data.R
@@ -16,10 +16,11 @@ hyperplane <- function(P,data,x,z=0) {
   (-c-a*x)/b
 }
 
-# setting initial number of observations
+# setting initial number of observations in each set
 n <- 200
 
 # calculating x-variable using two randomly generated sets with overlap
+set.seed(100)
 svcdatax1 <- matrix(rnorm(n*2, 0.5,0.3), ncol=2)
 svcdatax2 <- matrix(rnorm(n*2,-0.5,0.3), ncol=2)
 svcdatax <- rbind(svcdatax1,svcdatax2)
@@ -34,26 +35,50 @@ plot(svcdatax, col=(3-svcdatay),xlab="",ylab="",pch=16)
 abline(0,-1,col="gray",lty="dashed")
 
 # creating data frame 
-svcdata <- data.frame (x = svcdatax, y = as.factor (svcdatay))
+svcdata <- data.frame (x = svcdatax, y = factor(svcdatay))
 
 # computing svm function
-svmfit <- svm (svcdatay~., data = svcdata , kernel="linear", cost = 1000, scale = FALSE)
+svmfit <- svm (y~., data = svcdata , kernel="linear", cost = 1000, scale = FALSE)
 
-# plotting svm function
-plot(svmfit,svcdata)
-
-hyperplane(svmfit,dat,x,0)
-
-plt0 <- hyperplane(svmfit,dat,x,0)
-plt1 <- hyperplane(svmfit,dat,x,1)
-plt2 <- hyperplane(svmfit,dat,x,-1)
-
-plot(newx,col=(3-y),xlab="",ylab="",sub="Figure 2.1")
-lines(x,plt0,col='black')
-lines(x,plt1,col='gray')
-lines(x,plt2,col='gray')
-
+# looking at statistics
 summary(svmfit)
 svmfit$index
 svmfit$rho
 svmfit$coefs
+
+# plotting svm function graph
+plot(svmfit,svcdata)
+
+# calculating the hyperplane and supporting hyperplanes
+plt0 <- hyperplane(svmfit,svcdata,svcdatax,0)
+plt1 <- hyperplane(svmfit,svcdata,svcdatax,1)
+plt2 <- hyperplane(svmfit,svcdata,svcdatax,-1)
+
+# plotting the data with hyperplanes
+plot(svcdatax,col=(3-svcdatay),xlab="",ylab="",pch=16)
+lines(svcdatax,plt0,col='black')
+lines(svcdatax,plt1,col='gray')
+lines(svcdatax,plt2,col='gray')
+
+# using cost of 1
+svmfit1 <- svm (y~., data = svcdata , kernel="linear", cost = 1, scale = FALSE)
+summary(svmfit1)
+  # results in 36 support vectors
+
+# using cost of 100
+svmfit100 <- svm (y~., data = svcdata , kernel="linear", cost = 100, scale = FALSE)
+summary(svmfit100)
+  # results in 21 support vectors
+
+# using cost of 1000
+svmfit1000 <- svm (y~., data = svcdata , kernel="linear", cost = 1000, scale = FALSE)
+summary(svmfit1000)
+  # results in 20 support vectors
+
+# using cost of 10000
+svmfit10000 <- svm (y~., data = svcdata , kernel="linear", cost = 10000, scale = FALSE)
+summary(svmfit10000)
+  # results in 20 support vectors
+
+
+help(svm)
