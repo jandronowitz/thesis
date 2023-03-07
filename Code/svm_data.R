e6e8caa780a5598077851e9e4cfaada59b0ee8a8
Julia Andronowitz <juliaandronowitz@Julias-MacBook-Air.local>
2023-03-07 13:29:41 -0500

    adding figures and code for different costs/gammas

--- Code/svm_data.R
+++ Code/svm_data.R
@@ -45,7 +45,44 @@ plot(svmdatax, col=(11-svmdatay),xlab="",ylab="",pch=16)
 # creating data frame 
 svmdata <- data.frame (x = svmdatax, y = factor(svmdatay))
 
-# computing svm function
+# using different costs
+svmfit.00001 <- svm (y~., data = svmdata , kernel="radial", cost = 0.00001, scale = FALSE)
+svmfit.001 <- svm (y~., data = svmdata , kernel="radial", cost = 0.001, scale = FALSE)
+svmfit1 <- svm (y~., data = svmdata , kernel="radial", cost = 1, scale = FALSE)
+svmfit1000 <- svm (y~., data = svmdata , kernel="radial", cost = 1000, scale = FALSE)
+svmfit100000 <- svm (y~., data = svmdata , kernel="radial", cost = 100000, scale = FALSE)
+svmfit10000000 <- svm (y~., data = svmdata , kernel="radial", cost = 10000000, scale = FALSE)
+
+# plots of different costs
+plot(svmfit.00001,svmdata,color.palette = terrain.colors,symbolPalette = c("red","black","darkgray"))
+plot(svmfit.001,svmdata,color.palette = terrain.colors,symbolPalette = c("red","black","darkgray"))
+plot(svmfit1,svmdata,color.palette = terrain.colors,symbolPalette = c("red","black","darkgray"))
+plot(svmfit1000,svmdata,color.palette = terrain.colors,symbolPalette = c("red","black","darkgray"))
+plot(svmfit100000,svmdata,color.palette = terrain.colors,symbolPalette = c("red","black","darkgray"))
+plot(svmfit10000000,svmdata,color.palette = terrain.colors,symbolPalette = c("red","black","darkgray"))
+
+# using different gammas
+svmfit.00001g <- svm (y~., data = svmdata , kernel="radial", cost = 1, gamma = 0.00001, scale = FALSE)
+svmfit.001g <- svm (y~., data = svmdata , kernel="radial", cost = 1, gamma = 0.001, scale = FALSE)
+svmfit1g <- svm (y~., data = svmdata , kernel="radial", cost = 1, gamma = 1, scale = FALSE)
+svmfit1000g <- svm (y~., data = svmdata , kernel="radial", cost = 1, gamma = 1000, scale = FALSE)
+
+# plots of different gammas
+plot(svmfit.00001g,svmdata,color.palette = terrain.colors,symbolPalette = c("red","black","darkgray"))
+plot(svmfit.001g,svmdata,color.palette = terrain.colors,symbolPalette = c("red","black","darkgray"))
+plot(svmfit1g,svmdata,color.palette = terrain.colors,symbolPalette = c("red","black","darkgray"))
+plot(svmfit1000g,svmdata,color.palette = terrain.colors,symbolPalette = c("red","black","darkgray"))
+
+# using tune function to find the best model
+tune <- tune(svm,y~.,data=svmdata, 
+             ranges = list(gamma = 2^(-2:10), cost = 2^(-2:10)), tunecontrol = tune.control(sampling = "fix"))
+
+tune$best.parameters
+tune$best.performance
+ # tune$performances
+    # run to see individual performances
+
+# computing svm function with best gamma and cost
 svmfit <- svm (y~., data = svmdata , kernel="radial", cost = 0.25, gamma=0.5, scale = FALSE)
 
 # looking at statistics
@@ -57,26 +94,12 @@ svmfit$coefs
 # plotting svm function graph
 plot(svmfit,svmdata,color.palette = terrain.colors,symbolPalette = c("red","yellow","black"))
 
-# using different costs
-svmfit1 <- svm (y~., data = svmdata , kernel="radial", cost = 1, scale = FALSE)
-svmfit1000000 <- svm (y~., data = svmdata , kernel="radial", cost = 1000000, scale = FALSE)
-
-# plots of different costs
-plot(svmfit1,svmdata,color.palette = terrain.colors,symbolPalette = c("red","black","darkgray"))
-plot(svmfit1000000,svmdata,color.palette = terrain.colors,symbolPalette = c("red","black","darkgray"))
-
 # looking at how a,b,c values differ 
 hyperplane(svmfit,svmdata,svmdatax)
 hyperplane(svmfit1,svmdata,svmdatax)
 hyperplane(svmfit1000000,svmdata,svmdatax)
 
-# using tune function to find the best model
-tune <- tune(svm,y~.,data=svmdata, 
-     ranges = list(gamma = 2^(-2:10), cost = 2^(-2:10)), tunecontrol = tune.control(sampling = "fix"))
 
-tune$best.parameters
-tune$best.performance
-tune$performances
 help(tune)
 help(svm)
 
